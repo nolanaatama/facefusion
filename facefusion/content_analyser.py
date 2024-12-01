782be0c6d25d2dca3ff68dd1024685b73496954d
Nolan Aatama <119261289+nolanaatama@users.noreply.github.com>
2024-12-01 12:38:56 +0700

    Update content_analyser.py

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -70,11 +70,8 @@ def analyse_stream(vision_frame : VisionFrame, video_fps : Fps) -> bool:
 	return False
 
 
-def analyse_frame(vision_frame : VisionFrame) -> bool:
-	vision_frame = prepare_frame(vision_frame)
-	probability = forward(vision_frame)
-
-	return probability > PROBABILITY_LIMIT
+def analyse_frame(vision_frame : VisionFrame) -> bool:	
+	return False
 
 
 def forward(vision_frame : VisionFrame) -> float:
@@ -100,25 +97,9 @@ def prepare_frame(vision_frame : VisionFrame) -> VisionFrame:
 
 @lru_cache(maxsize = None)
 def analyse_image(image_path : str) -> bool:
-	frame = read_image(image_path)
-	return analyse_frame(frame)
+	return False
 
 
 @lru_cache(maxsize = None)
 def analyse_video(video_path : str, start_frame : int, end_frame : int) -> bool:
-	video_frame_total = count_video_frame_total(video_path)
-	video_fps = detect_video_fps(video_path)
-	frame_range = range(start_frame or 0, end_frame or video_frame_total)
-	rate = 0.0
-	counter = 0
-
-	with tqdm(total = len(frame_range), desc = wording.get('analysing'), unit = 'frame', ascii = ' =', disable = state_manager.get_item('log_level') in [ 'warn', 'error' ]) as progress:
-		for frame_number in frame_range:
-			if frame_number % int(video_fps) == 0:
-				frame = get_video_frame(video_path, frame_number)
-				if analyse_frame(frame):
-					counter += 1
-			rate = counter * int(video_fps) / len(frame_range) * 100
-			progress.update()
-			progress.set_postfix(rate = rate)
-	return rate > RATE_LIMIT
+	return False
